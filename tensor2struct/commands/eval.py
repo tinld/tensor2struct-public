7a245b3668e3980ade2d6d4aecec78c62586925b
tinld <19522348@gm.uit.edu.vn>
2022-11-11 16:01:38 +0700

    upload directioon of data

--- tensor2struct/commands/eval.py
+++ tensor2struct/commands/eval.py
@@ -37,6 +37,7 @@ def compute_metrics(config_path, config_args, section, inferred_path, etype, log
         inferred_path = inferred_path.replace("__LOGDIR__", logdir)
 
     inferred = open(inferred_path)
+    print(inferred)
     data = registry.construct("dataset", config["data"][section])
     metrics = data.Metrics(data, etype)
 
@@ -56,9 +57,11 @@ def compute_metrics(config_path, config_args, section, inferred_path, etype, log
 
         if etype in ["execution", "all"]:
             # if eval by execution, then we choose the first executable one from the beams
+            print(data)
             metrics.add_beams(data[infer_results["index"]], inferred_codes, data[i].orig["question"])
         else:
             assert etype in ["match", "sacreBLEU", "tokenizedBLEU"]
+            print(data)
             metrics.add_one(data[infer_results["index"]], inferred_codes[0])
     return logdir, metrics.finalize()
 
