24a288ff1a31e8593b064fb4dc7185cbb17ff5f1
tinld <19522348@gm.uit.edu.vn>
2022-11-11 14:48:26 +0700

    print bug subquerytree

--- tensor2struct/languages/ast/spider.py
+++ tensor2struct/languages/ast/spider.py
@@ -437,6 +437,7 @@ class SpiderUnparser:
         # nested query in from clause, recursively use the refinement
         if "from" in tree and tree["from"]["table_units"][0]["_type"] == "TableUnitSql":
             for table_unit in tree["from"]["table_units"]:
+                print(table_unit["s"])
                 subquery_tree = table_unit["s"]
                 self.refine_from(subquery_tree)
             return
