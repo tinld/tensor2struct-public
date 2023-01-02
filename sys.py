f56a1ad61ece60cd267466d90b2b19008f25d9e7
tinld <19522348@gm.uit.edu.vn>
2023-01-02 21:19:38 +0700

    update XLM config

---
+++ sys.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path[2] = '/usr/lib/python38.zip'
+sys.path[3] = '/usr/lib/python3.8'
+sys.path[4] = '/usr/lib/python3.8/lib-dynload'
+sys.path[5] = '/usr/local/lib/python3.8/dist-packages'
+sys.path[7] ='/usr/local/lib/python3.8/dist-packages/IPython/extensions'
