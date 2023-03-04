f3990488a7665072125ce0486bea31a79b2d8a26
tinld <19522348@gm.uit.edu.vn>
2023-03-04 08:28:08 +0700

    upload file viBERT4news

--- tensor2struct/modules/bert_tokenizer.py
+++ tensor2struct/modules/bert_tokenizer.py
@@ -29,10 +29,11 @@ class BERTokenizer:
             pretrained_tokenizer = AutoTokenizer.from_pretrained(version)
             pretrained_tokenizer.save_pretrained(vocab_dir)
         
-        if "uncased" in version or "cased" not in version:
+        if "uncased" in version:
             lowercase = True # roberta, electra, bert-base-uncased
         else:
             lowercase = False # bert-cased
+            
         if "phobert" or "vibert4news-base-cased" in version: 
             self.tokenizer = Tokenizer.from_pretrained(version)
             self.auto_tokenizer = AutoTokenizer.from_pretrained(version)
